fcb7e616287139b19f5d7821e115dfcd13da6173
leoxiaofei <xxffsoft@163.com>
2019-06-18 11:14:39 +0800

    搭建LCU Demo工程 增加复制粘贴的功能

---
+++ msserialize/msscenedef.h
@@ -0,0 +1,13 @@
+#ifndef MSSCENEDEF_H__
+#define MSSCENEDEF_H__
+
+namespace MSRPC
+{
+	enum ITEM_DATA_KEY { IDK_NAME, IDK_TRANSFORM, IDK_PIXMAP, IDK_SVG };
+
+	enum TRANSFORM_KEY { TK_TRANSLATE, TK_SCALE, TK_ROTATE, _TK_TOTAL_ };
+
+}
+
+
+#endif // MSSCENEDEF_H__
