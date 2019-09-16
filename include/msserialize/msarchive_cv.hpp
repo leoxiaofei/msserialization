28aa60f5aa8845b798fd991c6d68f9f51139a78e
xfguo <xfguo@zyhd.com.cn>
2019-09-16 21:48:09 +0800

    添加opencv常用数据结构解析。

---
+++ include/msserialize/msarchive_cv.hpp
@@ -0,0 +1,64 @@
+#ifndef MSARCHIVE_CV_H__
+#define MSARCHIVE_CV_H__
+
+#include "msarchive_stl.hpp"
+
+
+
+namespace MSRPC
+{
+	template<class NODE, class T>
+	class ISerialize<NODE, cv::Point_<T> >
+	{
+	public:
+		static void serialize(NODE& vNewNode, const cv::Point_<T>& tValue)
+		{
+			std::stringstream ss;
+			ss << tValue.x << ',' << tValue.y;
+			ISerialize<NODE, std::string>::serialize(vNewNode, ss.str());
+		}
+	};
+
+	template<class NODE, class T>
+	class OSerialize<NODE, cv::Point_<T> >
+	{
+	public:
+		static void serialize(NODE& vNewNode, cv::Point_<T>& tValue)
+		{
+			std::string strValue;
+			OSerialize<NODE, std::string>::serialize(vNewNode, strValue);
+			std::stringstream ss(strValue);
+			char ch(0);
+			ss >> tValue.x >> ch >> tValue.y;
+		}
+	};
+
+	template<class NODE, class T>
+	class ISerialize<NODE, cv::Rect_<T> >
+	{
+	public:
+		static void serialize(NODE& vNewNode, const cv::Rect_<T>& tValue)
+		{
+			std::stringstream ss;
+			ss << tValue.x << ',' << tValue.y << ',' << tValue.width << ',' << tValue.height;
+			ISerialize<NODE, std::string>::serialize(vNewNode, ss.str());
+		}
+	};
+
+	template<class NODE, class T>
+	class OSerialize<NODE, cv::Rect_<T> >
+	{
+	public:
+		static void serialize(NODE& vNewNode, cv::Rect_<T>& tValue)
+		{
+			std::string strValue;
+			OSerialize<NODE, std::string>::serialize(vNewNode, strValue);
+			std::stringstream ss(strValue);
+			char ch(0);
+			ss >> tValue.x >> ch >> tValue.y >> ch >> tValue.width >> ch >> tValue.height;
+		}
+	};
+
+}
+
+#endif // MSARCHIVE_CV_H__
