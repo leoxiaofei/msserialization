aacc72ffa7799003f9d81fbdda4fb8c275b9bc34
leoxiaofei <xxffsoft@163.com>
2020-04-14 13:46:46 +0800

    1、修正std::list模版序列化问题。 2、补充std::map模版序列化功能。

--- include/msserialize/msarchive_stl.hpp
+++ include/msserialize/msarchive_stl.hpp
@@ -103,10 +103,11 @@ namespace MSRPC
 		static void serialize(NODE& vNewNode, const std::list<T>& tValue)
 		{
 			vNewNode.set_array();
-			for (int ix = 0; ix != tValue.size(); ++ix)
+			for (std::list<T>::const_iterator itor = tValue.begin(); 
+				itor != tValue.end(); ++itor)
 			{
 				NODE vNode = vNewNode.new_node();
-				ISerialize<NODE, T>::serialize(vNode, tValue[ix]);
+				ISerialize<NODE, T>::serialize(vNode, *itor);
 				vNewNode.push_node(vNode);
 			}
 		}
@@ -126,6 +127,142 @@ namespace MSRPC
 				tValue.push_back(t);
 			}
 
+		}
+	};
+	
+	template<class NODE, class T>
+	class ISerialize<NODE, std::map<std::string, T> >
+	{
+	public:
+		static void serialize(NODE& vNewNode, const std::map<std::string, T>& tValue)
+		{
+			vNewNode.set_object();
+			for (typename std::map<std::string, T>::const_iterator citor = tValue.begin();
+				citor != tValue.end(); ++citor)
+			{
+				NODE vNode = vNewNode.new_node();
+				ISerialize<NODE, T>::serialize(vNode, citor->second);
+				vNewNode.add_member(citor->first.c_str(), vNode);
+			}
+		}
+	};
+
+	template<class NODE, class T>
+	class OSerialize<NODE, std::map<std::string, T> >
+	{
+	public:
+		static void serialize(const NODE& vNewNode, std::map<std::string, T>& tValue)
+		{
+			std::set<std::string> setKey;
+
+			typename NODE::ObjIter itor = vNewNode.sub_members();
+			for (; itor; ++itor)
+			{
+				if (NODE node = *itor)
+				{
+					T& t = val[itor.key()];
+					OSerialize<NODE, T>::serialize(node, t);
+					setKey.insert(itor.key());
+				}
+			}
+
+			if (setKey.size() != tValue.size())
+			{
+				for (std::map<std::string, T>::iterator itor = tValue.begin();
+					itor != tValue.end();)
+				{
+					if (setKey.find(itor->first) == setKey.end())
+					{
+						itor = tValue.erase(itor);
+					}
+					else
+					{
+						++itor;
+					}
+				}
+			}
+		}
+	};
+
+	template<class NODE, class K, class T>
+	class ISerialize<NODE, std::map<K, T> >
+	{
+	public:
+		static void serialize(NODE& vNewNode, const std::map<K, T>& tValue)
+		{
+			vNewNode.set_object();
+
+			if (!tValue.isEmpty())
+			{
+				NODE vKeyNode = vNewNode.new_node();
+				vKeyNode.set_array();
+
+				NODE vValueNode = vNewNode.new_node();
+				vValueNode.set_array();
+
+				for (typename std::map<K, T>::const_iterator citor = tValue.begin();
+					 citor != tValue.end(); ++citor)
+				{
+					NODE vKNode = vKeyNode.new_node();
+					ISerialize<NODE, K>::serialize(vKNode, citor->first);
+					vKeyNode.push_node(vKNode);
+
+					NODE vVNode = vValueNode.new_node();
+					ISerialize<NODE, T>::serialize(vVNode, citor->second);
+					vValueNode.push_node(vVNode);
+				}
+
+				vNewNode.add_member("key", vKeyNode);
+				vNewNode.add_member("value", vValueNode);
+			}
+		}
+	};
+
+	template<class NODE, class K, class T>
+	class OSerialize<NODE, std::map<K, T> >
+	{
+	public:
+		static void serialize(const NODE& vNewNode, std::map<K, T>& tValue)
+		{
+			NODE vKeyNode = vNewNode.sub_member("key");
+			NODE vValueNode = vNewNode.sub_member("value");
+
+			if (vKeyNode && vValueNode)
+			{
+				std::set<K> setKey;
+
+				typename NODE::ArrIter itorKey = vKeyNode.sub_nodes();
+				typename NODE::ArrIter itorValue = vValueNode.sub_nodes();
+				for (; itorKey && itorValue; ++itorKey, ++itorValue)
+				{
+					NODE nodeKey = *itorKey;
+					NODE nodeVal = *itorValue;
+					if (nodeKey && nodeVal)
+					{
+						K k;
+						OSerialize<NODE, K>::serialize(nodeKey, k);
+						T& t = tValue[k];
+						OSerialize<NODE, T>::serialize(nodeVal, t);
+						setKey.insert(k);
+					}
+				}
+
+				if (setKey.size() != tValue.size())
+				{
+					for (std::map<K, T>::iterator itor = tValue.begin();
+						itor != tValue.end();)
+					{
+						if (setKey.find(itor->first) == setKey.end())
+						{
+							itor = tValue.erase(itor);
+						}
+						else
+						{
+							++itor;
+						}
+					}
+				}
+			}
 		}
 	};
 
