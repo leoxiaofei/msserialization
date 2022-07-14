ad4f45a2876934759f24e3aceadfb1eee9b8a3c6
leoxiaofei <soft@leoxiaofei.com>
2022-07-14 16:54:15 +0800

    修正SiExSe参数不够的问题。（改为支持100个参数）

--- include/msserialize/msarchive.hpp
+++ include/msserialize/msarchive.hpp
@@ -591,7 +591,11 @@ namespace MSRPC
 	N, N, N, N, N, N, N, N, N, N, \
 	N, N, N, N, N, N, N, N, N, N, \
 	N, N, N, N, N, N, N, N, N, N, \
-	N, N, N, 1)
+	N, N, N, N, N, N, N, N, N, N, \
+	N, N, N, N, N, N, N, N, N, N, \
+	N, N, N, N, N, N, N, N, N, N, \
+	N, N, N, N, N, N, N, N, N, N, \
+	1)
 
 #define _MS_MACROARGCHECK(_0, \
 	_01, _02, _03, _04, _05, _06, _07, _08, _09, _10, \
@@ -600,7 +604,11 @@ namespace MSRPC
 	_31, _32, _33, _34, _35, _36, _37, _38, _39, _40, \
 	_41, _42, _43, _44, _45, _46, _47, _48, _49, _50, \
 	_51, _52, _53, _54, _55, _56, _57, _58, _59, _60, \
-	_61, _62, _63, TARGET, ...) TARGET
+	_61, _62, _63, _64, _65, _66, _67, _68, _69, _70, \
+	_71, _72, _73, _74, _75, _76, _77, _78, _79, _80, \
+	_81, _82, _83, _84, _85, _86, _87, _88, _89, _90, \
+	_91, _92, _93, _94, _95, _96, _97, _98, _99, _100, \
+	TARGET, ...) TARGET
 
 #define MS_ENUMARGS(MSFUNC,...) MS_EXPAND(MS_FOREACH(MSFUNC, __VA_ARGS__))
 
@@ -612,10 +620,14 @@ namespace MSRPC
 #define MS_EMPTY()
 #define MS_PROT(FUNC) FUNC MS_EMPTY()
 
-#define MS_EXPAND(...)  MS_EXPAND1(MS_EXPAND1(MS_EXPAND1(MS_EXPAND1(__VA_ARGS__))))
-#define MS_EXPAND1(...) MS_EXPAND2(MS_EXPAND2(MS_EXPAND2(MS_EXPAND2(__VA_ARGS__))))
-#define MS_EXPAND2(...) MS_EXPAND3(MS_EXPAND3(MS_EXPAND3(MS_EXPAND3(__VA_ARGS__))))
-#define MS_EXPAND3(...) __VA_ARGS__
+
+#define MS_EXPAND(...)  MS_EXPAND1(MS_EXPAND1(MS_EXPAND1(__VA_ARGS__)))
+#define MS_EXPAND1(...) MS_EXPAND2(MS_EXPAND2(MS_EXPAND2(__VA_ARGS__)))
+#define MS_EXPAND2(...) MS_EXPAND3(MS_EXPAND3(MS_EXPAND3(__VA_ARGS__)))
+#define MS_EXPAND3(...) MS_EXPAND4(MS_EXPAND4(MS_EXPAND4(__VA_ARGS__)))
+#define MS_EXPAND4(...) MS_EXPAND5(MS_EXPAND5(MS_EXPAND5(__VA_ARGS__)))
+#define MS_EXPAND5(...) MS_EXPAND6(MS_EXPAND6(MS_EXPAND6(__VA_ARGS__)))
+#define MS_EXPAND6(...) __VA_ARGS__
 
 
 #define SiArIo(MEM) ar.io(#MEM, tValue.MEM); \
