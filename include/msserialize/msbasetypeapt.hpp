2c8fd2d7dcd807c1b19a807cba392c161df77670
leoxiaofei <soft@leoxiaofei.com>
2024-12-08 22:06:37 +0800

    增加BaseTypeApt简化转换模板函数

--- include/msserialize/msbasetypeapt.hpp
+++ include/msserialize/msbasetypeapt.hpp
@@ -5,4 +5,17 @@ namespace MSRPC
 	template<typename S, typename T>
 	class BaseTypeApt;
 
+	template<typename S, typename T>
+	S T2S(const T& t)
+	{
+		return BaseTypeApt<S, T>(t);
+	}
+
+	template<typename S, typename T>
+	T S2T(const S& s)
+	{
+		T t;
+		(BaseTypeApt<S, T>(t)) = s;
+		return t;
+	}
 }
